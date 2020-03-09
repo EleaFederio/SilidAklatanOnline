86d93d5ea7abca9e0ddd94bd6df9fe85eb9edd8d
Eleazar Federio <eleazarfederio@rocketmail.com>
2020-03-09 10:54:15 +0800

    add default user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+        ]);
         // $this->call(UsersTableSeeder::class);
     }
 }
