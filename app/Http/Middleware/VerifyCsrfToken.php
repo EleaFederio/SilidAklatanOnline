3b1acd585463fe5f4a56ac7b61baff1601514863
EleazarFederio <eleazarfederio_@outlook.com>
2021-01-30 15:21:53 +0800

    *** try heroku domain ***

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -21,6 +21,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         'books/search',
         'api/login',
-        'logout'
+        'logout',
+        'student'
     ];
 }
